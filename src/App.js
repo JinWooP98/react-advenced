ff0340aab7b5ddd64cc89cfe312ddbf497289e96
JinWooP98 <qwdk0406@naver.com>
2024-07-26 09:41:23 +0900

    # 7. redux함수와 useSelector

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React, { useState } from "react";
+import ReduxCounter from "./redux-practice/components/ReduxCounter";
 
-import Counter from "./optimizing/components/Counter/Counter";
-import Header from "./optimizing/components/Header";
-import { log } from "./optimizing/log";
-import ConfigureCounter from "./optimizing/components/Counter/ConfigureCounter";
+
 
 const App = () => {
-    log("<App /> rendered");
-
-
-    const [chosenCount, setChosenCount] = useState(0);
-
-    const setCountHandler = number => {
-        setChosenCount(number);
-    };
 
     return (
         <>
-            <Header />
-            <main>
-                <ConfigureCounter onSet={setCountHandler}/>
-                {/*만약 Counter가 여러개일 경우 key에 같은 값이 들어가 다른 방법을 사용해야 함*/}
-                <Counter key={chosenCount} initialCount={chosenCount} />
-            </main>
+            <ReduxCounter />
         </>
     );
 };
