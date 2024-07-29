7d1f45fe54ca324d3cbc5025d33e57d73d2a35dc
JinWooP98 <qwdk0406@naver.com>
2024-07-29 09:46:43 +0900

    # 13. 카트 조건부 렌더링 상태관리

--- src/redux-cart/components/Cart/CartButton.js
+++ src/redux-cart/components/Cart/CartButton.js
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from './CartButton.module.css';
+import {useDispatch} from "react-redux";
+import {uiActions} from "../../store/ui-slice";
 
 const CartButton = () => {
 
+    const dispatch = useDispatch();
+
+    const toggleCartHandler = e => {
+        dispatch(uiActions.toggle());
+    };
+
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={styles.badge}>0</span>
     </button>
--- src/redux-cart/components/Layout/MainHeader.js
+++ src/redux-cart/components/Layout/MainHeader.js
@@ -1,8 +1,12 @@
 import React from "react";
 import CartButton from '../Cart/CartButton';
 import styles from './MainHeader.module.css';
+import {useSelector} from "react-redux";
+import {uiActions} from "../../store/ui-slice";
 
 const MainHeader = () => {
+
+
   return (
     <header className={styles.header}>
       <h1>ReduxCart</h1>
